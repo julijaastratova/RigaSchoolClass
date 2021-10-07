5d9b810a8d1d4c822a5c6b0eb3205dad28035c85
JuJu <JuJu@DESKTOP-ND643SE>
2021-10-07 15:53:34 +0300

    Add project files.

---
+++ RigaSchoolClass.UI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RigaSchoolClass.UI
+{
+	class Program
+	{
+		//this is a method
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello!");
+			Console.WriteLine("My name is Julija.");
+			Console.WriteLine("I enjoy travelling.");
+			Console.WriteLine("I also love long walks.");
+			Console.WriteLine("I like 70's-80's rock music.");
+			Console.WriteLine("I am enjoying this .net course, thank you!");
+			Console.ReadLine();
+			
+		}
+	}
+}
