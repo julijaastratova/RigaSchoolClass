ba16b960090497febcf18a668f1bef932296e42f
julijaastratova <j.sitkovska@gmail.com>
2021-10-13 18:06:36 +0300

    Homework2

---
+++ HomeWorkDataTypes/Programone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWorkDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal height = 1.65m;
+            decimal agePoint = 30.10m;
+            int age = 30;
+            DateTime date = DateTime.Now;
+            string name = "Julija";
+            string city = "Riga";
+          
+            Console.WriteLine("My name is " + name + ", my height is " + height + " cm.");
+            Console.WriteLine("I am " + agePoint + " years old, but in fact I am still " + (int)agePoint + ".");
+            Console.WriteLine("I am from " + city + ".");
+            Console.WriteLine("Today is " + date + ", thank you for your attention!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
--- RigaSchoolClass.UI/Program.cs
+++ RigaSchoolClass.UI/Program.cs
@@ -7,22 +7,87 @@ namespace RigaSchoolClass.UI
 		//this is a method
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello!");
-			Console.WriteLine("My name is Julija.");
-			Console.WriteLine("I enjoy travelling.");
-			Console.WriteLine("I also love long walks.");
-			Console.WriteLine("I like 70's-80's rock music.");
-			Console.WriteLine("I am enjoying this .net course, thank you!");
-			Console.WriteLine("new");
+
+			#region Casting
+
+			string strNumber = "111";
+
+			int resulfOfIntegerType = int.Parse(strNumber) + 1;
+
+			int integerVariable = 1;
+			long longVariable = 1222222;
+			decimal decimalVariable = 1.3m;
+			string stringVariable = "100";
+
+			string result = stringVariable + integerVariable.ToString();
+			Console.WriteLine($"Casting result: {result}");
+
+			int intdecimalVariable = (int)decimalVariable;
+			int intlongVariable = (int)decimalVariable;
+		
+			Console.WriteLine($"Decimal casting: {intdecimalVariable}");
+			Console.WriteLine($"Long casting: {intlongVariable}");
+
+
+
+
+            #endregion
+
+			#region IntegerSampleRegion
+
+			
+			int number1 = 100;
+			int number2 = 1;
+
+			int intresult = number1 + number2;
+
+			string number3 = "100";
+			string number4 = "1";
+
+			string stringResult = number3 + number4; 
+
+			
+
+			Console.WriteLine($"int result: {intresult}");
+			Console.WriteLine($"string result: {stringResult}");
+
+			#endregion
+
+			#region StringSampleRegion
+
+			string helloWorldVariable = "Hello World line 1";
+			string helloWorldVariableNumber2 = "Hello World line 2";
+			
+			//Line 1
+			Console.WriteLine("Line 1: " + helloWorldVariable);
+			Console.Write(Environment.NewLine);
+			
+			//Line 2
+			Console.WriteLine("Line 2: " + helloWorldVariableNumber2);
+			Console.WriteLine(Environment.NewLine);
+
+			//Line 3
+			Console.WriteLine("Line 3: " + helloWorldVariable + helloWorldVariableNumber2 + "end of the line");
+			Console.Write(Environment.NewLine);
+
+			//Line 4
+			Console.WriteLine($"Line 4: {helloWorldVariable} {helloWorldVariableNumber2} end of the line");
+			Console.WriteLine(Environment.NewLine);
+
+			//Line 5
+			Console.WriteLine(string.Format("Line 5: {0} {1} {2}", helloWorldVariable, helloWorldVariableNumber2, "end of the line"));
+
 			Console.ReadLine();
 
-			//Comment
-			//Comment
-			//Comment
-			//Hellohowareyou
-			//newchanges
-			//this is a change
-			
-		}
-	}
+            //Comment
+            //Comment
+            //Comment
+            //Hellohowareyou
+            //newchanges
+            //this is a change
+
+            #endregion
+
+        }
+    }
 }
